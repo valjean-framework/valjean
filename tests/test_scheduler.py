a44793427ff773ab01a9b5b0bf65fe1d39a852d2
Davide Mancusi <davide.mancusi@cea.fr>
2017-10-03 16:38:01 +0200

    Improve scheduler tests

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import random
+from hypothesis import given, settings
+from hypothesis.strategies import lists, floats, integers, just
 
 import context
 from velvet.depgraph import DepGraph
@@ -10,36 +12,46 @@ from velvet.task import DelayTask
 
 class TestScheduler:
 
-    def test_many_small_indep_tasks_many_workers(self):
-        self.run(n_tasks=1000, n_workers=1000, task_duration=1e-6, dep_fraction=0.0)
-
-    def test_many_small_indep_tasks_few_workers(self):
-        self.run(n_tasks=1000, n_workers=5, task_duration=1e-6, dep_fraction=0.0)
-
-    def test_many_small_dep_tasks_many_workers(self):
-        self.run(n_tasks=1000, n_workers=1000, task_duration=1e-6, dep_fraction=0.01)
-
-    def test_many_small_dep_tasks_few_workers(self):
-        self.run(n_tasks=1000, n_workers=5, task_duration=1e-6, dep_fraction=0.01)
-
-    def test_few_smallish_dep_tasks_few_workers(self):
-        self.run(n_tasks=6, n_workers=4, task_duration=1.0, dep_fraction=0.1)
-
-    def test_one_task_many_workers(self):
-        self.run(n_tasks=1, n_workers=1000, task_duration=1.0, dep_fraction=0.0)
-
-    def run(self, n_tasks, n_workers, task_duration, dep_fraction):
-        task_deps, all_tasks = self.create_tasks(n_tasks, task_duration, dep_fraction)
-        g = DepGraph(task_deps)
-        s = Scheduler(g, QueueScheduling(n_workers=n_workers, sleep_interval=0.01))
-        s.schedule()
-
-    def create_tasks(self, n_tasks, task_duration, dep_fraction):
+    @staticmethod
+    def create_tasks(task_durations, dep_frac):
         task_deps = {}
         all_tasks = []
-        for i in range(n_tasks):
+        for i, task_duration in enumerate(task_durations):
             task = DelayTask(str(i), task_duration)
-            dependees = [t for t in all_tasks if random.random() < dep_fraction]
-            task_deps[task] = dependees
+            if dep_frac>0.0:
+                dependees = [t for t in all_tasks if random.random() < dep_frac]
+                task_deps[task] = dependees
+            else:
+                task_deps[task] = []
             all_tasks.append(task)
-        return task_deps, all_tasks
+        return task_deps
+
+    @given(task_durations=lists(floats(min_value=1e-15, max_value=1e-5),
+                                average_size=100)
+           .map(lambda ls: TestScheduler.create_tasks(ls, 0.0)),
+           n_workers=integers(min_value=1, max_value=100))
+    def test_indep_tasks(self, task_durations, n_workers):
+        self.run(task_durations, n_workers)
+
+    @given(task_durations=lists(just(0.0), average_size=50)
+           .map(lambda ls: TestScheduler.create_tasks(ls, 0.02)),
+           n_workers=integers(min_value=1, max_value=100))
+    def test_dep_tasks(self, task_durations, n_workers):
+        self.run(task_durations, n_workers)
+
+    @given(tasks=lists(floats(min_value=0.1, max_value=1.0),
+                       average_size=6)
+           .map(lambda ls: TestScheduler.create_tasks(ls, 0.1)),
+           n_workers=integers(min_value=2, max_value=6))
+    @settings(max_examples=5, deadline=10000)
+    def test_few_smallish_dep_tasks_few_workers(self, tasks, n_workers):
+        self.run(tasks, n_workers)
+
+    def test_one_task_many_workers(self):
+        self.run(TestScheduler.create_tasks([1.0], 0.0), 1000)
+
+    def run(self, task_deps, n_workers):
+        g = DepGraph(task_deps)
+        s = Scheduler(g, QueueScheduling(n_workers=n_workers,
+                                         sleep_interval=1e-5))
+        s.schedule()
