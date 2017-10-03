f49e356055ae9bc939eda0b3386ee50cb209105a
Davide Mancusi <davide.mancusi@cea.fr>
2017-10-03 16:37:29 +0200

    Remove randomness from DelayTask

--- velvet/task.py
+++ velvet/task.py
@@ -8,7 +8,6 @@ Any class with a :meth:`do()` method qualifies as a Task.
 '''
 
 import time
-import random
 import logging
 
 
@@ -17,16 +16,27 @@ logger.setLevel(logging.WARNING)
 
 
 class DelayTask:
-    def __init__(self, name, average=1.):
+    '''Task that waits for the specified number of seconds.
+
+    This task is useful to test scheduling algorithms under different load
+    conditions.
+
+    :param name str: The name of the task
+    :param delay float: The amount of time (in seconds) that this task will
+                        wait when executed.
+    '''
+
+    def __init__(self, name, delay=1.):
         self.name = name
-        self.average = float(average)
+        self.delay = float(delay)
 
     def __repr__(self):
         return '"DelayTask {}"'.format(self.name)
 
     def do(self):
-        lapse = random.expovariate(1./self.average)
+        '''Perform the task (i.e. sleep; I wish my life was like that).'''
+
         logger.info('DelayTask %s sleeping %f seconds...',
-                    self, lapse)
-        time.sleep(lapse)
+                    self, self.delay)
+        time.sleep(self.delay)
         logger.info('DelayTask %s waking up!', self)
