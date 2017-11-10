99ab64e0dc3b74abd26c4f16ecb069ee1f5f86cd
Davide Mancusi <davide.mancusi@cea.fr>
2017-11-10 18:23:56 +0100

    Fix flake8 warnings

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,11 @@
 def pytest_addoption(parser):
     parser.addoption("--valjean-verbose", action="store_true",
-        help="Maximize valjean verbosity")
+                     help="Maximize valjean verbosity")
+
 
 def pytest_generate_tests(metafunc):
     if metafunc.config.getoption('valjean_verbose'):
-        from .context import valjean
+        from .context import valjean  # noqa: F401
         from valjean import set_log_level
         import logging
         set_log_level(logging.DEBUG)
