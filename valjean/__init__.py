2fa1716338bcb78dc9f8e8e256961792f8964167
Davide Mancusi <davide.mancusi@cea.fr>
2017-11-16 15:25:12 +0100

    Add package-wide get_log_level function

--- valjean/__init__.py
+++ valjean/__init__.py
@@ -4,11 +4,19 @@ _cosette = ['depgraph', 'task', 'task.task', 'task.code', 'scheduler']
 __all__ = list(map(lambda x: 'cosette.' + x, _cosette))
 __version__ = '0.1'
 
+log_level = logging.INFO
+
 logging.basicConfig(format='%(levelname)s (%(name)s) %(asctime)s: %(message)s',
-                    level=logging.INFO)
+                    level=log_level)
 
 
 def set_log_level(level):
+    global log_level
     for module in __all__:
         logger = logging.getLogger(__name__ + '.' + module)
         logger.setLevel(level)
+        log_level = level
+
+
+def get_log_level():
+    return log_level
