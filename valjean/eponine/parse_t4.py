f4de7fcc0d15bbf373c6a0645e3acab7f4123269
Eve Le Ménédeu <eve.le-menedeu@cea.fr>
2018-01-31 15:43:05 +0100

    Deals with the Pyparsing exceptions.

--- valjean/eponine/parse_t4.py
+++ valjean/eponine/parse_t4.py
@@ -13,6 +13,11 @@ import valjean.eponine.pyparsing_t4.grammar as pygram
 from pprint import pprint
 import Enjolras
 from pyparsing import ParseResults
+from pyparsing import ParseException
+import logging
+
+
+LOGGER = logging.getLogger('valjean')
 
 # in Eponine, profile is a key of globals
 if 'profile' not in globals().keys():
@@ -21,12 +26,9 @@ if 'profile' not in globals().keys():
         return fprof
 
 
-class EndListingError(Exception):
-    '''Listing did not finished by NORMAL COMPLETION and/or had no result
-    block.
-    '''
+class T4ParserException(Exception):
+    '''An error that may be raised by the :class:`T4Parser` class.'''
     pass
-
 
 class T4Parser():
     '''Parse Tripoli-4 listings.
@@ -34,7 +36,7 @@ class T4Parser():
 
     @profile
     def __init__(self, jddname, batch=-1, endflag="simulation time",
-                 meshlim=1000, para=False):
+                 meshlim=-1, para=False): #config, *, 
         self.jdd = jddname
         self.batch_number = batch
         self.end_flag = endflag
@@ -48,50 +50,130 @@ class T4Parser():
     def scan_t4_listing(self):
         '''Scan Tripoli-4 listing, calling :mod:`.scan_t4`'''
         self.scan_res = scan_t4.Scan(self.jdd, self.end_flag, self.mesh_lim)
-        if not self.scan_res.normalend and len(self.scan_res) == 0:
-            raise EndListingError
+        print("is scan_res ?", self.scan_res)
+        print("len(scan_res) =", len(self.scan_res))
+        print("normal end:", self.scan_res.normalend)
+        # need to look if we keep or not the excpetion, catch it,
+        # let it crash... -> how to count unsuccessful jobs...
+        if not self.scan_res:
+            raise T4ParserException("No result found in Tripoli-4 listing.")
+        if not self.scan_res.normalend:
+            LOGGER.warning("Tripoli-4 listing did not finished with "
+                           "NORMAL COMPLETION.")
 
     def parse_t4_listing(self):
-        '''Parse Tripoli-4 results, calling pypasing and :mod:`.pyparsing_t4`
+        '''Parse Tripoli-4 results, calling pyparsing and :mod:`.pyparsing_t4`
         '''
-        str_to_parse = ""
         if self.batch_number == 0:
             str_to_parse = self.scan_res.get_all_batch_results()
         else:
-            str_to_parse = self.scan_res[self.batch]
+            str_to_parse = self.scan_res[self.batch_number]
+        if LOGGER.isEnabledFor(logging.DEBUG):
+            with open("jddres.txt", 'w') as fout:
+                fout.write(str_to_parse)
         # now parse the results string
         start_time = time.time()
         try:
-            self.result = pygram.mygram.parseString(self.strres)
-        except:
-            LOGGER.warning("Parsing failed, please look carefully to the "
-                           "output to understand why.")
-            # sys.exit()
+            self.result = pygram.mygram.parseString(str_to_parse)
+        except ParseException:
+            LOGGER.warning("Parsing failed, you are probably trying to read a "
+                           "new response. Please implement it before "
+                           "re-running.")
+            LOGGER.warning("If you want to get the pyparsing failure message, "
+                           "please run with LOGGER at level debug.")
+            if LOGGER.isEnabledFor(logging.DEBUG):
+                raise
+            # exception should be caught somewhere ?
+            # from None allows to raise a new exception without traceback and
+            # message of the previous one here.
+            raise T4ParserException("Error in parsing, see above.") from None
         print("[31mTemps:", time.time()-start_time, "[0m")
 
     def print_t4_stats(self):
         '''Print Tripoli-4 statistics (warnings and errors).'''
         self.scan_res.print_statistics()
 
+    def check_t4_times(self):
+        '''Check if running times are well written in Tripoli-4 listings.
+        These times are at the end of the result block and mark the end flag.
+
+        :returns: boolean, True if well present, else False
+
+        Returned bool depends on the listing:
+
+        * if the job was run in parallel mode (and declared so),
+          ``"simulation time"`` and ``"elapsed time"`` should be present in
+          that order, only the second one is checked
+        * if the listing name contains ``"exploit"`` or ``"verif"``, it is most
+          probably an exploitation job (Green bands for example),
+          ``"exploitation time"`` is checked
+        * else ``"simulation time"`` is checked
+        '''
+        if self.result:
+            if isinstance(self.result[-1], dict):
+                return ((self.para and "elapsed_time" in self.result[-1])
+                        or (not self.para
+                            and ('simulation_time' in self.result[-1]
+                                 or 'exploitation_time' in self.result[-1])))
+            return ((self.para and "elapsed_time" in self.result)
+                    or (not self.para and 'simulation_time' in self.result))
+
     def print_t4_times(self):
         '''Print time characteristics of the Tripoli-4 result considered.
         This print includes initialization time, simulation time, exploitation
         time and elapsed time.
         '''
-        print("initialization time:", self.scan_res.initialization_time)
-        print("end times:")
-        if self.result:
+        print("Initialization time:", self.scan_res.initialization_time)
+        if self.check_t4_times():
             if isinstance(self.result[-1], dict):
-                if (('simulation_time' in self.result[-1]
-                     or (self.para and "elpased time" in self.result[-1]))):
-                    return True
-                elif 'exploitation_time' in self.result[-1]:
-                    return True
+                if self.para:
+                    print("Elapsed time:", self.result[-1]['elapsed_time'])
+                if "exploitation_time" in self.result[-1]:
+                    print("Exploitation time:",
+                          self.result[-1]['exploitation_time'])
                 else:
-                    return False
+                    print("Simulation time",
+                          self.result[-1]['simulation_time'])
             else:
-                if (('simulation_time' in self.result
-                     or (self.para and "elpased time" in self.result))):
-                    return True
-                else:
-                    return False
+                if self.para:
+                    print("Elapsed time:", self.result['elapsed_time'])
+                if 'simulation_time' in self.result:
+                    print("Simulation time:", self.result['simulation_time'])
+
+def main(myjdd="", mode="MONO"):
+    if myjdd == "":
+        try:
+            myjdd = sys.argv[1]
+        except IndexError:
+            print("Eponine: argument needed (jdd name)")
+            exit(-1)
+    print(myjdd)
+    print("mode =", mode)
+    # mode = "PARA"
+
+    myendflag = "simulation time"
+    # if "exploit" in myjdd:
+    if "exp" in myjdd and "verif" not in myjdd:
+        myendflag = "exploitation time"
+    if mode == "PARA":
+        myendflag = "elapsed time"
+    print("endflag =", myendflag)
+
+    t4_res = T4Parser(myjdd, endflag=myendflag, meshlim=2) #, para=True)
+    try:
+        t4_res.scan_t4_listing()
+    except T4ParserException:
+        print("scan failed")
+    else:
+        t4_res.print_t4_stats()
+        print("result of the function =", t4_res.check_t4_times())
+        t4_res.print_t4_times()
+        try:
+            t4_res.parse_t4_listing()
+        except T4ParserException as t4pe:
+            print(t4pe)
+        else:
+            print("successful parsing")
+
+if __name__ == "__main__":
+    main()
