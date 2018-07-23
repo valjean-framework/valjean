207985ef1e2c5668cea88b2226a27979beb7aae2
Eve Le Ménédeu <eve.le-menedeu@cea.fr>
2018-07-23 15:19:55 +0200

    Print dataset bins and add unit

--- valjean/eponine/dataset.py
+++ valjean/eponine/dataset.py
@@ -3,6 +3,8 @@
 
 from collections import namedtuple
 import numpy as np
+import pprint
+PP = pprint.PrettyPrinter(indent=4, depth=1)
 
 
 class Dataset:
@@ -11,7 +13,7 @@ class Dataset:
 
     Data = namedtuple('Data', ['value', 'error'])
 
-    def __init__(self, data, bins, name, title):
+    def __init__(self, data, bins, name, title='', unit='unknown'):
         # data = namedTuple, values, errors
         # name = spectrum, mesh, integrated_res, etc.
         assert isinstance(data, Dataset.Data)
@@ -20,15 +22,21 @@ class Dataset:
         self.bins = bins
         self.name = name
         self.title = title
+        self.unit = unit
 
     def __repr__(self):
         if isinstance(self.data.value, np.ndarray):
             print(type(self.data.value), type(self.data.error))
-            return ("Dataset (name: {0}, value: {1}, error: {2})\n"
+            return ("Dataset name: {0},\n"
+                    "        value: {1},\n"
+                    "        error: {2},\n"
+                    "        bins: {3}\n"
+                    "        unit: '{4}'\n"
                     .format(self.name, self.data.value.squeeze(),
-                            self.data.error.squeeze()))
-        return ("Dataset (name: {0}, value: {1:6e}, error: {2:6e})\n"
-                .format(self.name, self.data.value, self.data.error))
+                            self.data.error.squeeze(), self.bins, self.unit))
+        return (
+            "Dataset name: {0}, value: {1:6e}, error: {2:6e}, unit: '{3}'\n"
+            .format(self.name, self.data.value, self.data.error, self.unit))
 
 
 def RelativelyEqual(ds1, ds2, tolerance=1e-5):
