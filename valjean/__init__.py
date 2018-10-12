d89a42ea48657a608b8d1499ec0c4a773e4544e9
Davide Mancusi <davide.mancusi@cea.fr>
2018-10-12 17:35:15 +0200

    Make LOGGER use different formats for INFO messages and other messages

--- valjean/__init__.py
+++ valjean/__init__.py
@@ -10,9 +10,32 @@ except DistributionNotFound:
     __version__ = 'unknown'
 
 
+class ValjeanFormatter:
+    '''Custom formatter for log messages.
+
+    This formatter class uses a different format for the ``INFO`` message
+    level.
+    '''
+
+    def __init__(self, info_fmt, fmt):
+        '''Initialize the formatter.
+
+        :param str info_fmt: The format string for the ``INFO`` level.
+        :param str fmt: The format string for the other message levels.
+        '''
+        self.info_formatter = logging.Formatter(info_fmt)
+        self.formatter = logging.Formatter(fmt)
+
+    def format(self, record):
+        '''Format the given record.'''
+        if record.levelno == logging.INFO:
+            return self.info_formatter.format(record)
+        return self.formatter.format(record)
+
+
 def _configure_logger(logger):
-    default_level = logging.WARNING
-    _formatter = logging.Formatter(LOG_CONSOLE_FORMAT)
+    default_level = logging.INFO
+    _formatter = ValjeanFormatter(LOG_INFO_CONSOLE_FORMAT, LOG_CONSOLE_FORMAT)
     _handler = logging.StreamHandler(sys.stdout)
     _handler.setFormatter(_formatter)
     _handler.setLevel(default_level)
@@ -21,6 +44,7 @@ def _configure_logger(logger):
 
 
 LOGGER = logging.getLogger('valjean')
+LOG_INFO_CONSOLE_FORMAT = '* %(message)s'
 LOG_CONSOLE_FORMAT = '%(levelname)9.9s %(module)12.12s: %(message)s'
 LOG_FILE_FORMAT = ('%(levelname)9.9s (%(module)10.10s/%(funcName)12.12s) '
                    '%(asctime)19s: %(message)s')
