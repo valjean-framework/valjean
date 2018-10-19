0d7be3c12d540787ed2f6239ef10cd2461ae35b4
Eve Le Ménédeu <eve.le-menedeu@cea.fr>
2018-10-19 16:29:10 +0200

    Documentation and doctest of dataset module.
    Also add a named argument for squeeze to be able to name the new dataset.

--- valjean/eponine/dataset.py
+++ valjean/eponine/dataset.py
@@ -1,4 +1,78 @@
 '''Common module to structure data in same way for all codes.
+
+A dataset is, up to now, composed a 4 members:
+
+  * value: a :obj:`numpy.ndarray` or a :obj:`numpy.generic` (scalar from numpy
+    represented like the arrays, with dim, etc)
+  * error: an object of same type as value
+  * bins: an :obj:`collections.OrderedDict` (optional and named argument)
+  * name (optional and named argument)
+
+The bins object should have the same dimension as the value, the order matches
+the dimensions. If no bins are available it is still possible to use an empty
+:obj:`collections.OrderedDict`.
+
+>>> from valjean.eponine.dataset import Dataset
+>>> import numpy as np
+>>> from collections import OrderedDict
+>>> vals = np.arange(10).reshape(2, 5)
+>>> errs = np.array([1]*10).reshape(2, 5)
+>>> bins = OrderedDict([('bacon', np.arange(3)), ('egg', np.arange(5))])
+>>> len(bins) == vals.ndim
+True
+>>> myds = Dataset(vals, errs, bins=bins)
+>>> len(myds.bins) == myds.value.ndim
+True
+>>> isinstance(myds.error, type(myds.value))
+True
+>>> myds.error.shape == myds.value.shape
+True
+
+If there are useless dimensions, the dataset can be squeezed:
+
+>>> vals = np.arange(6).reshape(1, 2, 1, 3)
+>>> errs = np.array([0.1]*6).reshape(1, 2, 1, 3)
+>>> bins = OrderedDict([('bacon', np.array([0, 1])),
+...                     ('egg', np.array([0, 2, 4])),
+...                     ('sausage', np.array([10, 20])),
+...                     ('spam', np.array([-5, 0, 5, 10]))])
+>>> ds = Dataset(vals, errs, bins=bins, name="ds_to_squeeze")
+>>> ds.value.shape == (1, 2, 1, 3)
+True
+>>> len(ds.bins) == 4
+True
+>>> np.array_equal(ds.value, np.array([[[[0, 1, 2]], [[3, 4, 5]]]]))
+True
+>>> sds = ds.squeeze()
+>>> sds.value.ndim == 2
+True
+>>> len(sds.bins) == 2
+True
+>>> sds.value.shape == (2, 3)
+True
+>>> list(len(x)-1 for x in sds.bins.values())  # edges of bins, so N+1
+[2, 3]
+>>> list(sds.bins.keys()) == ['egg', 'spam']
+True
+>>> np.array_equal(sds.value, np.array([[0, 1, 2], [3, 4, 5]]))
+True
+
+The dimensions with only one bin are squeezed, the same is done on the bins.
+
+It is possible to give a new name to the squeezed dataset, else the default one
+is kept.
+
+>>> ds.name == "ds_to_squeeze"
+True
+>>> sds.name == "ds_to_squeeze"
+True
+>>> nsds = ds.squeeze(name="squeezed")
+>>> nsds.name == "squeezed"
+True
+>>> np.array_equal(nsds.value, sds.value)
+True
+>>> nsds.bins == sds.bins
+True
 '''
 
 import logging
@@ -16,23 +90,32 @@ class Dataset:
     .. todo::
 
         Think about units. Possibility: using a units package from scipy.
+
+    .. todo::
+
+        How to deal with bins of N values (= center of bins)
     '''
 
     def __init__(self, value, error, *, bins=OrderedDict(), name=''):
-        # data = namedTuple, values, errors
-        # name = spectrum, mesh, integrated_res, etc.
-        # self.value et self.data.value pointent bien au meme endroit...
-        # possibilite d'ajouter un check sur le bins eux-memes (N+1 bin par elt
-        # shape)
+        '''Dataset class initialization.
+
+        :param value: array of N dimensions representing the values
+        :type value: :obj:`numpy.ndarray` or :obj:`numpy.generic`
+        :param error: array of N dimensions representing the **absolute**
+          errors
+        :type error: :obj:`numpy.ndarray` or :obj:`numpy.generic`
+        :param bins: bins corresponding to value (named optional parameter)
+        :type bins: :obj:`collections.OrderedDict` (str, :obj:`numpy.ndarray`)
+        :param str name: name of the dataset (named optional parameter)
+
+        For the moment, N+1 values in the arrays corresponding to bins edges.
+        '''
         assert isinstance(value, (np.ndarray, np.generic))
         assert isinstance(bins, OrderedDict)
         LOGGER.debug("Number of dimensions from bins: %s", len(bins))
         LOGGER.debug("Type of value: %s", type(value))
         LOGGER.debug("Value charac: ndim = %s, shape = %s, size = %s",
                      value.ndim, value.shape, value.size)
-        # last or foressen for scalar as ndim == 0
-        # assert (len(bins) == value.ndim or not bins
-        #         or (value.ndim == 0 and len(bins) == 1))
         assert value.shape == error.shape, \
             "Value and error do not have the same shape"
         assert len(bins) == value.ndim or not bins
@@ -64,7 +147,7 @@ class Dataset:
                 'bins': self.bins,
                 'name': self.name}
 
-    def squeeze(self):
+    def squeeze(self, name=None):
         '''Squeeze dataset: remove useless dimensions.
 
         Squeeze is based on the shape and on the bins dim ??? To confirm...
@@ -78,8 +161,10 @@ class Dataset:
         for axis, dim in enumerate(shape):
             if dim < 2:
                 lbins.pop(key_axis[axis])
-        return self.__class__(self.value.squeeze(), self.error.squeeze(),
-                              bins=lbins, name=self.name)
+        return self.__class__(self.value.squeeze(),
+                              self.error.squeeze(),
+                              bins=lbins,
+                              name=self.name if name is None else name)
 
 
 def relatively_equal(ds1, ds2, tolerance=1e-5):
