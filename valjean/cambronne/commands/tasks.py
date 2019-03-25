862e0e649c0601ac1c1b1d2bb4866cce8e359548
Davide Mancusi <davide.mancusi@cea.fr>
2019-03-25 19:08:33 +0100

    Implement the 'names' subcommand for the 'tasks' command

--- valjean/cambronne/commands/tasks.py
+++ valjean/cambronne/commands/tasks.py
@@ -15,20 +15,35 @@ class TasksCommand(Command):
         '''Register options for this command in the parser.'''
         env_subparsers = parser.add_subparsers(title='tasks subcommands',
                                                dest='tasks_command')
+        names_help = ('Print the names of the tasks in the environment to the '
+                      'screen.')
+        names_cmd = env_subparsers.add_parser('names', help=names_help)
+        names_cmd.set_defaults(func=self.names)
+
         show_help = ('Print the environment to the screen. If a task name is '
                      'specified, print only the corresponding environment '
                      'section.')
         show_cmd = env_subparsers.add_parser('show', help=show_help)
-        rm_help = 'Remove the given section from the environment.'
-        rm_cmd = env_subparsers.add_parser('rm', help=rm_help)
         show_cmd.add_argument('name', metavar='NAME', nargs='?',
                               help='The name of the environment section to '
                               'show.')
+        show_cmd.set_defaults(func=self.show)
+
+        rm_help = 'Remove the given section from the environment.'
+        rm_cmd = env_subparsers.add_parser('rm', help=rm_help)
         rm_cmd.add_argument('name', metavar='NAME',
                             help='the name of the environment section to '
                             'delete.')
-        show_cmd.set_defaults(func=self.show)
         rm_cmd.set_defaults(func=self.remove)
+
+    # pylint: disable=no-self-use
+    def names(self, args, _collected_tasks, _config):
+        '''Execute the ``tasks names`` command.'''
+        # deserialize the environment
+        env = init_env(path=args.env_path, skip_read=False,
+                       fmt=args.env_format)
+
+        print('\n'.join(env.keys()))
 
     # pylint: disable=no-self-use
     def show(self, args, _collected_tasks, _config):
