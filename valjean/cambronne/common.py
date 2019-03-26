8c1917098ea6ffb0043f98f92215d1d49eb8bbed
Davide Mancusi <davide.mancusi@cea.fr>
2019-03-26 09:40:30 +0100

    Add diagnostic messages at the end of the run

--- valjean/cambronne/common.py
+++ valjean/cambronne/common.py
@@ -5,15 +5,8 @@ import argparse
 from ..cosette.depgraph import DepGraph
 from ..cosette.env import Env
 from ..cosette.scheduler import Scheduler
+from ..cosette.task import TaskStatus
 from .. import LOGGER
-
-
-class UniqueAppendAction(argparse.Action):
-    ''':mod:`argparse` action that stores option arguments in a set instead of
-    a list (no duplicates).'''
-    def __call__(self, parser, namespace, values, option_string=None):
-        unique_values = set(values)
-        setattr(namespace, self.dest, unique_values)
 
 
 class Command:
@@ -35,13 +28,72 @@ class Command:
         LOGGER.debug('building graph for tasks: %s', collected_tasks)
         graph = build_graph(collected_tasks)
         LOGGER.debug('resulting graph: %s', graph)
+        LOGGER.info('graph contains %s tasks', len(graph))
 
         env = init_env(path=args.env_path, skip_read=args.env_skip_read,
                        fmt=args.env_format)
         new_env = schedule(graph, env=env, config=config)
+
+        self.task_diagnostics(graph=graph, env=new_env, config=config)
+
         if not args.env_skip_write:
             write_env(env=env, path=args.env_path, fmt=args.env_format)
         return new_env
+
+    @classmethod
+    def task_diagnostics(cls, *, graph, env, config):
+        '''Emit diagnostic messages about the status of the tasks. Count how
+        many have succeeded, how many have failed, etc.  If any tasks have
+        failed, this method writes their names in a file called 'failed_tasks'
+        in the log directory.
+
+        :param DepGraph graph: the dependency graph.
+        :param Env env: the environment.
+        :param Config config: the configuration object.
+        '''
+        from collections import Counter
+        count_status = Counter()
+        missing = []
+        failed = []
+        for task in graph.nodes():
+            task_name = task.name
+            if task_name in env:
+                status = env[task.name]['status']
+                count_status[status] += 1
+                if status == TaskStatus.FAILED:
+                    failed.append(task_name)
+            else:
+                missing.append(task_name)
+
+        if missing:
+            LOGGER.warning('the following %s tasks are missing from the '
+                           'environment: %s',
+                           len(missing), '\n  '.join(missing))
+
+        total_graph = len(graph)
+        msgs = ('{:>7}: {}/{} ({:5.1f}%)'
+                .format(status.name, count, total_graph, 100*count/total_graph)
+                for status, count in count_status.items())
+        LOGGER.info('final environment statistics:\n  %s',
+                    '\n  '.join(msgs))
+
+        cls.write_failed_tasks(failed=failed, config=config)
+
+    @classmethod
+    def write_failed_tasks(cls, *, failed, config):
+        '''Write the names of the failed tasks in the ``failed_tasks`` file.
+
+        :param list(str) failed: the names of the failed tasks.
+        :param Config config: the configuration object.
+        '''
+        from pathlib import Path
+        if not failed:
+            return
+        log_root = config.get('path', 'log-root')
+        failed_fname = Path(log_root, 'failed-tasks')
+        with failed_fname.open('w') as failed_file:
+            for task in failed:
+                failed_file.write(task + '\n')
 
 
 def build_graph(tasks):
@@ -112,3 +164,11 @@ def schedule(graph, *, env, config=None):
     new_env = scheduler.schedule(env=env, config=config)
     LOGGER.debug('resulting environment: %s', new_env)
     return new_env
+
+
+class UniqueAppendAction(argparse.Action):
+    ''':mod:`argparse` action that stores option arguments in a set instead of
+    a list (no duplicates).'''
+    def __call__(self, parser, namespace, values, option_string=None):
+        unique_values = set(values)
+        setattr(namespace, self.dest, unique_values)
