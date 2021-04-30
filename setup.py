98ddf10253b7bbea5c8a905ae1c877f9b7c81bc3
Davide Mancusi <davide.mancusi@cea.fr>
2021-04-30 18:33:11 +0200

    Switch to poetry for build management

--- setup.py
+++ setup.py
@@ -31,54 +31,7 @@
 # The fact that you are presently reading this means that you have had
 # knowledge of the CeCILL license and that you accept its terms.
 
-from setuptools import setup, find_packages, __version__ as st_version
-from pkg_resources import parse_version
+import setuptools
 
-st_version_required = '12.0'
-st_version_required_parsed = parse_version(st_version_required)
-if parse_version(st_version) < st_version_required_parsed:
-    message = ('ERROR: installation of some dependencies requires '
-               'setuptools >= {}; your version is {}.\n'
-               'Run `pip install --upgrade setuptools` first.'
-               .format(st_version_required, st_version))
-    raise ImportError(message)
-
-name = 'valjean'
-author = u'Ève Le Ménédeu, Davide Mancusi'
-author_email = u'davide.mancusi@cea.fr'
-copyright = u'2017-2020, ' + author
-
-def read_req(filename):
-    with open(filename) as req:
-        return list(line.strip() for line in req)
-
-install_reqs = read_req('requirements.txt')
-graphviz_reqs = read_req('requirements_graphviz.txt')
-testing_reqs = read_req('requirements_testing.txt')
-doc_reqs = read_req('requirements_doc.txt')
-lint_reqs = read_req('requirements_lint.txt')
-dev_reqs = graphviz_reqs + testing_reqs + doc_reqs + lint_reqs
-
-setup(name=name,
-      author=author,
-      author_email=author_email,
-      description="VALidation, Journal d'Évolution et ANalyse",
-      url=r'https://codev-tuleap.intra.cea.fr/projects/valjean',
-      packages=find_packages(exclude=['doc', 'tests', 'tests.*']),
-      package_data={'': ['conf.py.template', 'valjean.css',
-                         '*.C', '*.h']},
-      python_requires='>=3.5',
-      setup_requires=['setuptools_scm'],
-      install_requires=install_reqs,
-      extras_require={
-          'dev': dev_reqs,
-          'graphviz': graphviz_reqs
-          },
-      data_files=[('', ['README.md'])],
-      use_scm_version=True,
-      entry_points={
-          'console_scripts': [
-              'valjean = valjean.cambronne.main:main'
-              ]
-          }
-      )
+if __name__ == "__main__":
+    setuptools.setup()
