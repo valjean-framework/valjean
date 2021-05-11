22511871b63637d9e058b82e77edcd145a2a4f2f
Davide Mancusi <davide.mancusi@cea.fr>
2021-05-11 18:51:03 +0200

    Adapt ci/spack-python.sh

--- ci/spack-python.sh
+++ ci/spack-python.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
+umask 002
 SPACK_ROOT=$1
 if [ "x${SPACK_ROOT}" = x ]; then
   echo "usage: source spack-python.sh <SPACK_ROOT>"
   exit 1
 fi
 
-source "${SPACK_ROOT}/share/spack/setup-env.sh"
+SPACK=$SPACK_ROOT/bin/spack
+
 # generate a sanitized environment name (only letters, digits, - and _ allowed)
-ENV_NAME=$(spack arch -o)
+OS=$(${SPACK} arch -o)
 if [ "x${SUFFIX}" != x ]; then
-  ENV_NAME="${ENV_NAME}-${SUFFIX}"
+  ENV_NAME="${OS}-${SUFFIX}"
+else
+  ENV_NAME="${OS}"
 fi
 ENV_NAME=$(echo "${ENV_NAME}" | tr -d '\n' | tr -c '[:alnum:]_-' _)
 
-# load all the python modules; expecting to find them in the environment folder
-for module in ${SPACK_ROOT}/var/spack/environments/${ENV_NAME}/modules-python@*; do
-  source ${module}
-done
+# load all the python modules; expecting to find the script in the environment
+# folder
+source /etc/profile.d/modules.sh
+module use ${SPACK_ROOT}/share/spack/modules/linux-${OS}-x86_64
+source ${SPACK_ROOT}/var/spack/environments/${ENV_NAME}/loads
