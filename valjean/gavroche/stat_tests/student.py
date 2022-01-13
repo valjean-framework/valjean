Fix a bug the calculation of the Student p-value
This commit also adds tests for the p-values of the Student and Chi²
tests.

valjean/gavroche/stat_tests/student.py
@@ -28,68 +28,41 @@
 # The fact that you are presently reading this means that you have had
 # knowledge of the CeCILL license and that you accept its terms.
 
-r'''Statistic tests for datasets comparison.
-
+r'''
 .. _scipy: https://docs.scipy.org/doc/scipy/reference/index.html
 
-This module provides calculation and results for Student's t-test.
+In Student's t-test, we consider 2 different datasets that can have different
+means and different variances. The main problem is to test if the means are
+equal.  One way to do that is to use Student's t-test, although technically
+this name should be reserved for the case where the variances are unknown but
+they are known to be equal (the generalized version to different variances is
+called Welch's test).
 
-
-General Student's t-test
-````````````````````````
-
-In this test, we consider 2 different datasets that can have different means
-and different variances. The main problematic is to test the mean equality.
-Student's t-test is normally dedicated to equal (but unknown) variances, else
-the Welch test might be used (base on the same calculation but different
-hypothesis). For example, when the same quantity is determined by two different
-method, we use a Welch test as we cannot suppose equal variances. Both tests
-have to follow a Student law.
-
-The test formula is:
+In Tripoli-4, like in most Monte Carlo codes, the estimated statistical
+uncertainty is the standard error, i.e. the standard deviation of the mean of
+the samples. It is typically assumed that the distribution of the mean is
+normal, as a consequence of the central limit theorem. The variance of the
+population is estimated as
 
 .. math::
 
-    t_{Student} = \frac{\bar{x}_1 - \bar{x}_2}
-    {\sqrt{\frac{N_1\sigma_1^2 + N_2\sigma_2^2}{n_1 + n_2 -2}(\frac{1}{n_1}
-    + \frac{1}{n_2})}}
+    s^2 = \frac{1}{n-1}\sum_{i=1}^n(x_i - \bar{x})^2
 
-with :math:`\bar{x}_i` the mean of dataset *i*, :math:`\sigma_i` its standard
-deviation and :math:`n_i` the size of the sample (i.e. the number of batches
-used in simulation). :math:`N_i` also represents the size of the sample but
-with the number used in variance calculation, :math:`n_i` or :math:`n_i-1`.
-This only have consequences in exact calculation and will be negligeable in the
-following.
-
-Simplifications by :math:`n_i-1` lead to:
+and the variance of the mean is estimated as
 
 .. math::
 
-    t_{Student} = \frac{\bar{x}_1 - \bar{x}_2}
-    {\sqrt{\frac{\sigma_1^2}{n_1} + \frac{\sigma_2^2}{n_2}}}
+    \bar{s}^2 = \frac{s^2}{n}
 
+The standard error :math:`\bar{s}` is the quantity that typically enters
+Student's t-test.
 
-Student's t-test implementation in MC case
-``````````````````````````````````````````
-
-In Tripoli-4, like in most Monte Carlo codes, the σ that is calculated already
-includes :math:`n`, the number of batches as it measures the convergence of the
-mean (consequence from Central-limit theorem). The variance of the :math:`x_i`
-quantity is calculated thanks to
+The Student test statistic is:
 
 .. math::
 
-    \sigma^2 = \frac{1}{n-1}\sum_{i=1}^n(x_i - \bar{x})^2
-
-Then the variance of the mean is :math:`\frac{\sigma^2}{n}`, the quantity used
-in the Student's t-test. This is why :math:`n` is not required in our test.
-
-The formula used for the test is then:
-
-.. math::
-
-    t_{Student} = \frac{\bar{x}_1 - \bar{x}_2}
-    {\sqrt{\sigma_1^2 + \sigma_2^2}} = \Delta
+    t_{\text{Student}} = \frac{\bar{x}_1 - \bar{x}_2}
+    {\sqrt{\bar{s}_1^2 + \bar{s}_2^2}}
 
 
 Note that, with our :class:`~valjean.eponine.dataset.Dataset`, this is
@@ -98,7 +71,7 @@ equivalent to
 .. code::
 
     diff = dataset_1 - dataset_2
-    t(Student) = Δ = diff.value / diff.error
+    t(Student) = diff.value / diff.error
 
 as value is difference of values and error is equal to the quadratic sum of the
 errors.
@@ -110,94 +83,81 @@ Test interpretation
 The tested hypothesis in the Student's t-test, i.e. the null hypothesis is:
 
 * equal means, so :math:`m_1 = m_2`
-* gaussian errors (or error following a normal distribution)
+* Gaussian errors (or error following a normal distribution)
 * independent datasets.
 
 The test interpretation is based on the comparison of the p-value with a given
-significance level, usually the first kind error probability or α-error. The
-p-value is defined by :math:`P(|X| \geq |\Delta|)`, :math:`X` being the test
-statistic distribution (:math:`t`) for :math:`N` degrees of freedom. The
-α-error is given by the user, ususally 1 % or 5 %. We thus test:
-:math:`P(|X| \geq |\Delta|) \leq \alpha`. If this is the case the null
-hypothesis is rejected.
+significance level, called :math:`\alpha`. The (two-sided) p-value is defined
+as the probability to observe results more extreme than those that were
+actually observed, assuming that the null hypothesis is true. Mathematically,
+:math:`p=P(|X| \geq |t_{\text{Student}}|\,|N)`, :math:`X` being the Student
+test statistic for samples of size :math:`N`; the variable :math:`N` is also
+called the number of degrees of freedom.  The significativity level
+:math:`\alpha` is given by the user, usually 1% or 5%.  Student's test fails if
+:math:`P(|X| \geq |t_{\text{Student}}|) \leq \alpha`. If this is the case the
+null hypothesis is rejected.
 
-To evaluate the test we thus need an estimation of the number of degrees of
-freedom, :math:`n_1 + n_2 -2` for the Student's t-test. In most cases we do
-compare results obtained from the same number of batches, so :math:`n_1 = n_2`,
-but this is not the most important: we usually have :math:`n_i \geq 1000`. This
-is why we can consider :math:`n_i\rightarrow\infty` in the Student tables. Our
-results are then considered close to a normal distribution.
+To evaluate the test we thus need an estimate of the number of degrees of
+freedom, which is typically approximated as :math:`n_1 + n_2 - 2`, with
+:math:`n_1` and :math:`n_2` being the number of batches. For large values of
+:math:`n_i` (i.e. for a large number of degrees of freedom), the distribution
+of the Student statistic approaches a standard normal distribution.
 
-In such case (and in many other) it looks easier to compare directly the value
-of Δ with a threshold corresponding to the value of the Student distribution
-for the limit on α required and the number of degrees of freedom of the test.
+It is sometimes easier to evaluate if the test succeeds by directly comparing
+the value of :math:`t_{\text{Student}}` with a threshold given by the
+significativity level :math:`\alpha` and the number of degrees of freedom.
 
-For example, here are some thresholds for some α values and a corresponding
-number of degrees of freedom:
+For example, here are some thresholds for some values of :math:`\alpha` and of
+the number of degrees of freedom:
 
 .. |inf| replace:: :math:`\infty`
 
-+------------+---------+---------+-------------------+-------------------+
-|            | α = 1 % | α = 5 % | α = 1 % (1-sided) | α = 5 % (1-sided) |
-+------------+---------+---------+-------------------+-------------------+
-|  N = 1000  | 2.5807  | 1.9623  |      -2.3300      |     -1.6464       |
-+------------+---------+---------+-------------------+-------------------+
-| N =  |inf| | 2.5758  | 1.9600  |      -2.3263      |     -1.6449       |
-+------------+---------+---------+-------------------+-------------------+
++------------+----------------------+----------------------+
+|            | :math:`\alpha` = 1 % | :math:`\alpha` = 5 % |
++------------+----------------------+----------------------+
+|  N = 10    |        2.7638        |        1.8125        |
++------------+----------------------+----------------------+
+|  N = 1000  |        2.5807        |        1.9623        |
++------------+----------------------+----------------------+
+|  N = |inf| |        2.5758        |        1.9600        |
++------------+----------------------+----------------------+
 
-When not precised α are two-sided, i.e. a symmetric distribution is considered
-and acceptance is the middle of the curve (see below). Results for N = |inf|
-come from the normal distribution.
-
-Using directly the threshold is not necessarly obvious, so the requirement is
-to choose a limit on the p-value, so on α, the threshold is then automatically
-calculated.
+Results for N = |inf| come from the normal distribution.
 
 
 .. note::
 
-    This test can also be used on histograms (spectrum, mesh): it will be
-    calculated per bin but it cannot be interpreted for the whole histogram, so
-    will raise an error. To use the bins individually a loop is needed, to get
-    an overall result a Bonferroni or a Holm-Bonferroni test can be applied
-    (see module bonferroni).
-
-.. todo::
-
-    Update link when Bonferroni method will be there...
-
+    This test can also be used on histograms (spectrum, mesh). In this case, an
+    independent test is made for each bin. The test for the whole histogram
+    succeeds if *all* the individual tests succeed.  A better option is to use
+    a Bonferroni or a Holm-Bonferroni test (see the :mod:`~.bonferroni`
+    module).
 
 Code implementation
 ```````````````````
 
-The Student's t-test statistic is calculated for each pair of datasets. It is
-per default compared to the threshold defined by a first kind error or α-error
-equal to 1 %, so, considering an infinite number of degrees (as it is
-usually at least 1000), a threshold around 2.576, obtained from the normal
-distribution (see :func:`TestStudent.student_threshold`).
+If the number of degrees of freedom is given, p-values are estimated using
+Student's t-distribution. If the number is not given or it is `None`, it is
+assumed that it is very large and the normal approximation is used.
 
-If the number of degrees of freedom is given, it is calculated from the Student
-distribution.
+The p-value is only calculated if the number of degrees of freedom is given;
+otherwise, the Student result is just compared to the threshold. To test the
+p-value instead of the value of the t-statistic, use
+:func:`TestResultStudent.test_pvalue`.
 
-P-value is only calculated if the number of degrees of freedom is given, else
-the Student result is just compared to the threshold. To test the p-value
-instead of the Δ value, use :func:`TestResultStudent.test_pvalue`.
-
-All these methods use the **SciPy** package, see `scipy`_. The Student
-distribution is used from :obj:`scipy.stats.t`, where the pdf is explicitely
+All these methods use the **SciPy** package (see `scipy`_). The Student
+distribution comes from :obj:`scipy.stats.t`, where the pdf is explicitely
 written.
 
 The method ``sf`` of :obj:`scipy.stats.t` corresponds to :math:`1 - cdf`, so
-give the p-value, while ``ppf``, the Percent point function, returns the
-threshold corresponding to the required probability (α-error for example).
-
-All calculation are done for a two-sided distribution, this is why we test
-:math:`|\Delta|` instead of :math:`\Delta` and why we use :math:`\alpha/2`
-instead of :math:`\alpha` to get the threshold.
+this gives the one-sided p-value; we need to multiply it by 2.0 to get the
+two-sided p-value. The ``ppf`` (percent point) function returns the threshold
+corresponding to the required one-sided significativity, so we need to divide
+:math:`\alpha` by 2.0 in our case.
 
 The methods where the test is really applied are static methods, they can be
-used outside of the :class:`TestStudent` class. Examples will only be shown
-inside this class, as this is the more expected behaviour.
+used outside of the :class:`TestStudent` class. Examples are given in the class
+docstrings.
 
 
 Examples
@@ -273,9 +233,8 @@ Since some of the comparisons failed here, the whole tests fails as well:
 >>> bool(tstudent_res)
 False
 
-Like in float case it is possible to ask for the p-values. In both cases the
-probability can be changed via the ``alpha`` argument, shown here in an array
-case:
+As in the scalar case, it is possible to ask for the p-values. In both cases
+the significativity can be changed via the ``alpha`` argument, as shown here:
 
 >>> tstudent = TestStudent(ds3, ds5, name="comp", alpha=0.05, ndf=1000,
 ...                        description="Comparison using Student's t-test")
@@ -287,19 +246,19 @@ case:
 >>> print(f'{tstudent_res.delta[0][1]:.7f}')
 -2.2283441
 
-This last value is in-between the thresholds at 1 % and 5 %, so accepted in the
-first case and rejected in the second one.
+This last value is in-between the thresholds at 1% and 5%, in magnitude, and
+therefore the test succeeds in the former case and fails in the latter.
 
 Obtaining p-values and comparing them is also possible in the spectrum case:
 
 >>> print(np.array2string(tstudent_res.pvalue[0],
 ...                       formatter={'float_kind':'{:.7e}'.format}))
-[3.2740885e-01 1.3039640e-02 5.0738755e-07 4.1155447e-01 8.0627620e-04]
+[6.5481769e-01 2.6079281e-02 1.0147751e-06 8.2310894e-01 1.6125524e-03]
 >>> print(np.array2string(tstudent_res.test_pvalue()[0]))
 [ True False False  True False]
 
-Finally, an array of size 1 (and dimension 1), the test will do the same as for
-datasets containing :obj:`numpy.generic`:
+Finally, for an array of size 1 (and dimension 1), the test will do the same as
+for datasets containing :obj:`numpy.generic`:
 
 >>> ds6 = Dataset(np.array([5.3]), np.array([0.2]))
 >>> ds7 = Dataset(np.array([5.25]), np.array([0.08]))
@@ -314,10 +273,8 @@ datasets containing :obj:`numpy.generic`:
 >>> bool(tstudent_res)
 True
 
-The format of the NumPy object stays the dataset one, expecially for the Δ.
-
 Like for usual operations on datasets, only tests between datasets of the same
-format are possible:
+shape are possible:
 
 >>> tstudent = TestStudent(ds1, ds7, name="comp",
 ...                        description="Comparison using Student's t-test")
@@ -326,7 +283,7 @@ Traceback (most recent call last):
     ...
 ValueError: Datasets to subtract do not have same shape
 
-Multiple dimensions datasets are also allowed:
+Multi-dimensional datasets are also allowed:
 
 >>> ds8 = Dataset(np.array([[5.2, 5.3, 5.25], [5.4, 5.5, 5.2]]),
 ...               np.array([[0.2, 0.25, 0.1], [0.2, 0.3, 0.1]]))
@@ -353,8 +310,7 @@ True
 .. warning::
     If the errors are equal to 0, so the Student denominator is 0, AND the
     values are equal, so the numerator is also 0, the Student value is set to
-    0. In that case the comparison is considered as OK (boolean returning
-    True).
+    0. In that case the test is assumed to succeed (boolean returning `True`).
 
     >>> ds10 = Dataset(np.array([3.2, 0, 5]), np.array([0, 0.5, 0]))
     >>> ds11 = Dataset(np.array([3.2, 0, 5.2]), np.array([0, 0.5, 0]))
@@ -438,15 +394,12 @@ class TestResultStudent(TestResult):
     def test_pvalue(self):
         '''Result of the test by testing p-value.
 
-        The limit on accepted probability is divided by 2 due to two-sided
-        test.
-
         :returns: bool or :obj:`numpy.ndarray` of bool if ``ndf`` was given to
                  :class:`TestStudent` and datasets are :obj:`numpy.ndarray`
         '''
         if self.test.ndf is None:
             return False
-        return [pval > self.test.alpha/2 for pval in self.pvalue]
+        return [pval > self.test.alpha for pval in self.pvalue]
 
 
 class TestStudent(TestDataset):
@@ -528,7 +481,7 @@ class TestStudent(TestDataset):
     def pvalue(delta, ndf):
         '''Calculation of the p-value (**static method**).
 
-        :param delta: Δ (Student's t-test statistic)
+        :param delta: Student's t-test statistic
         :type delta: :obj:`numpy.generic` (:obj:`float`) or
                      :obj:`numpy.ndarray` (:obj:`float`)
         :param ndf: number of degrees of freedom
@@ -537,36 +490,30 @@ class TestStudent(TestDataset):
                   :obj:`numpy.ndarray` (:obj:`float`)
         '''
         if ndf is None:
-            return norm.sf(np.fabs(delta))
-        return t.sf(np.fabs(delta), ndf)
+            return 2.0*norm.sf(np.fabs(delta))
+        return 2.0*t.sf(np.fabs(delta), ndf)
 
     @staticmethod
     def student_threshold(alpha, ndf=None):
-        '''Get threshold from probability (**static method**).
+        r'''Get threshold from probability (**static method**).
 
-        :param alpha: required significance level (α)
+        :param alpha: required significance level (:math:`\alpha`)
         :type alpha: float or :obj:`numpy.generic` (:obj:`float`)
         :param ndf: number of degrees of freedom
         :type ndf: int or :obj:`numpy.generic` (:obj:`int`), default is
                    ``None``
         :returns: :obj:`numpy.generic` (:obj:`float`)
 
-        Threshold considered is the 2-sided one, so probability has to be
-        decrease by half its value (1 half per side) and only the
-        :math:`|value|` is returned. This calculation is OK as the distribution
-        is symmetric.
+        The threshold is two-sided.
 
         If number of degrees of freedom is given, the distribution used is the
-        Student one corresponding to ``ndf``.
-
-        If not, we consider an infinite number of degrees of freedom (what is
-        often the case as we usually have at least 1000 batches). In that case,
-        as the Student distribution tends to a normal one when the number of
-        degrees of freedom is big, we use the normal distribution instead.
+        Student distribution corresponding to ``ndf``.  If not, we consider an
+        infinite number of degrees of freedom.  In that case, the Student
+        distribution tends to a normal distribution.
         '''
         if ndf is None:
-            return np.fabs(norm.ppf(alpha/2))
-        return np.fabs(t.ppf(alpha/2, ndf))
+            return np.fabs(norm.ppf(0.5*alpha))
+        return np.fabs(t.ppf(0.5*alpha, ndf))
 
     def data(self):
         '''Generator yielding objects supporting the buffer protocol that (as a